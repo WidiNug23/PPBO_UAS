ebbeb99e6e40aa2c93d7f75a0f71824db65c6eec
Widi Suryo Nugroho <127074841+WidiNug23@users.noreply.github.com>
2023-06-23 22:51:10 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author User
- */
-
 // Class Induk/Kelas Super
 class Manusia {
-    protected String nama;
+    private String nama;
 
     public Manusia(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
         this.nama = nama;
     }
 
@@ -33,6 +31,14 @@ class Mahasiswa extends Manusia {
         super(nama);
         this.nim = nim;
     }
+// Menggunakan get
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
 
     @Override
     public void info() {
@@ -42,7 +48,7 @@ class Mahasiswa extends Manusia {
 
     @Override
     public void sapa() {
-        System.out.println("Halo, nama saya " + nama + " (mahasiswa)");
+        System.out.println("Halo, nama saya " + getNama() + " (mahasiswa)");
     }
 }
 
@@ -55,25 +61,28 @@ class Pembicara {
 
 public class Main {
     public static void main(String[] args) {
-        // Membuat objek manusia dan mahasiswa
-        Manusia manusia = new Manusia("Widi");
-        Mahasiswa mahasiswa = new Mahasiswa("Widi Suryo Nugroho", "V3922047");
+        // Menggunakan try catch
+        try {
+            // Membuat objek manusia dan mahasiswa
+            Manusia manusia = new Manusia("Widi");
+            Mahasiswa mahasiswa = new Mahasiswa("Widi Suryo Nugroho", "V3922047");
 
-        // Memanggil metode info() dan sapa() dari objek manusia dan mahasiswa
-        manusia.info(); // Output: Nama: Widi
-        manusia.sapa(); // Output: Halo, nama saya Widi
+            // Memanggil metode info() dan sapa() dari objek manusia dan mahasiswa
+            manusia.info(); // Output: Nama: Widi
+            manusia.sapa(); // Output: Halo, nama saya Widi
 
-        mahasiswa.info(); // Output: Nama: Widi Suryo Nugroho
-                          //         NIM: V3922047
-        mahasiswa.sapa(); // Output: Halo, nama saya Widi Suryo Nugroho (mahasiswa)
+            mahasiswa.info(); // Output: Nama: Widi Suryo Nugroho
+            //         NIM: V3922047
+            mahasiswa.sapa(); // Output: Halo, nama saya Widi Suryo Nugroho (mahasiswa)
 
-        System.out.println();
+            System.out.println();
 
-        // Menggunakan polymorphism dengan objek manusia dan mahasiswa
-        Pembicara pembicara = new Pembicara();
-        pembicara.sapa(manusia); // Output: Halo, nama saya Widi
-        pembicara.sapa(mahasiswa); // Output: Halo, nama saya Widi Suryo Nugroho (mahasiswa)
+            // Menggunakan polymorphism dengan objek manusia dan mahasiswa
+            Pembicara pembicara = new Pembicara();
+            pembicara.sapa(manusia); // Output: Halo, nama saya Widi
+            pembicara.sapa(mahasiswa); // Output: Halo, nama saya Widi Suryo Nugroho (mahasiswa)
+        } catch (Exception e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
+        }
     }
 }
-
-
